06738a55a8dc5027bb7a18161c741712a3f5de53
takasimamasahiro <134473651+takasimamasahiro@users.noreply.github.com>
2024-06-07 21:55:13 +0900

    [add]カメラ移動の追加。

---
+++ Assets/takasima/Script/Camera.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSample : MonoBehaviour
+{
+
+   [SerializeField] GameObject _player;
+    Vector3 _offset;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //unitychan‚Ìî•ñ‚ğæ“¾
+        _player = GameObject.Find("Player");
+
+        // MainCamera(©•ª©g)‚Æplayer‚Æ‚Ì‘Š‘Î‹——£‚ğ‹‚ß‚é
+         _offset = transform.position - _player.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = _player.transform.position + _offset;
+
+    }
+}
