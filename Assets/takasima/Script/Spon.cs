d343a78547c2dcdf525fe40f6e7c879906ab1fce
takasimamasahiro <134473651+takasimamasahiro@users.noreply.github.com>
2024-06-16 15:45:33 +0900

    [update]　アイテムの生成が要件と違うとUIがないのを修正した

--- Assets/takasima/Script/Spon.cs
+++ Assets/takasima/Script/Spon.cs
@@ -7,13 +7,14 @@ public class Spon : MonoBehaviour
     [SerializeField] GameObject SponEnemy;
     public int _nums;
     public  Vector3 _position;
+    public int _interval = 2;
     // Start is called before the first frame update
     void Start()
     {
 
         for (int i = 0; i < _nums; i++)
         {
-           _position = new Vector3( _position.x -5, _position.y, _position.z );
+           _position = new Vector3( _position.x - _interval, _position.y, _position.z );
             Instantiate(SponEnemy, _position , Quaternion.identity);
         }
 
